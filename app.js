c787bd0a3644f5a9b7e1d1cbfd50a3e8866a2db1
Youssefelsawy <yosefelsawy406@gmail.com>
2023-03-27 17:40:01 +0200

    fixing body-parser

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
     })
     .catch(err => {console.log(err)});
 })
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
