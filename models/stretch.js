f0a8042f5e6909fe1d584113509b0b0f339f44fc
Youssefelsawy <yosefelsawy406@gmail.com>
2023-04-18 18:20:32 +0200

    adding Stretch Model

---
+++ models/stretch.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const stretch_Schema = new Schema({
+    name: {
+        type: String,
+        required: true
+      },
+      bodyPart: {
+        type: String,
+        required: true
+      },
+      imageUrl: {
+        type: String,
+        required: true
+      },
+      duration: {
+        type: Number,
+        required: true
+      }
+})
+
+module.exports = mongoose.model('stretches', stretch_Schema);
