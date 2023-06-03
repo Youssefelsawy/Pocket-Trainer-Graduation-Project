731b75b7799a56f6e8520a68a9ae5e86765e7b60
Youssefelsawy <yosefelsawy406@gmail.com>
2023-06-03 18:47:52 +0300

    changing

--- controllers/adding-exercise-workoutplan.js
+++ controllers/adding-exercise-workoutplan.js
@@ -2,26 +2,20 @@ const Exercise = require('../models/exercise')
 
 exports.addChestExercise = (req, res, next) => {
 const exeId = req.body.exerciseId
-Exercise.findOne({ _id: exeId })
-  .then(exercise => {
-    if (exercise) {
-      return req.user.addToChestDay(exercise);
-    } else {
-      res.status(400).send("Exercise does not exist");
-    }
-  })
-  .then(result => {
-    if (exist) {
-      res.status(200).send('This exercise is already added in your workoutPlan');
-    } else {
-      res.status(201).send(result);
-    }
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).send("An error occurred");
-  });
-
+    Exercise.findById(exeId)
+    .then(exercise => {
+        return req.user.addToChestDay(exercise)
+    }).then(result => {
+        if(exist) {
+            res.status(200).send('this exercise already added in your workoutPlan');
+        } else {
+            res.status(201).send(result)
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).send("Exercise you provide does not exist")
+      });
     exist = false;
 };
 
