11439c72e9a3a7e00907fb987cb0d9718a1514ac
Youssefelsawy <yosefelsawy406@gmail.com>
2023-06-14 05:22:32 +0300

    err

--- models/user.js
+++ models/user.js
@@ -116,7 +116,7 @@ const userSchema = new Schema({
 
 
 //forget password
-UserSchema.methods.createPasswordResetToken = function (){
+userSchema.methods.createPasswordResetToken = function (){
     const resetToken = crypto.randomBytes(32).toString('hex');
     this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
     this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
