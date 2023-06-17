f8fd33b9c76219a989cb20ff9e0f7ad50632c465
Youssefelsawy <yosefelsawy406@gmail.com>
2023-06-17 19:05:54 +0300

    trying

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
+// const app = express();
+
+const upload = multer({
+    storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+      },
+      filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+      }
+    })
+  });
 
 const authinticationController = require('../controllers/auth');
 
